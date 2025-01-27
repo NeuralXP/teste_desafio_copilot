99c68c27112ceabb167d9aa118f491158727346e
Heitor Exposito de Sousa <heitorexposito.ds@outlook.com>
2025-01-27 21:42:48 +0000

    geracao do primeiro codigo do desafio

---
+++ concatenar_dados.py
@@ -0,0 +1,13 @@
+# Receber dois valores do usuário e concatena-las
+
+# Receber o primeiro valor
+
+info_1 = input("Digite o primeiro valor: ")
+
+# Receber o segundo valor
+
+info_2 = input("Digite o segundo valor: ")
+
+# Exibir o resultado
+
+print("O resultado da concatenação é: ", info1 + info2)
